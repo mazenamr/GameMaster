d1cda3b34c2aa06ab541c99c3692830bcff309b4
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-05 00:26:14 +0200

    Add Initial Project Files

---
+++ GameMaster.Seeding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameMaster.Seeding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
