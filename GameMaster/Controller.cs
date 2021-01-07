e5331407f3757cc86eef48e31b936da35eb0d73f
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-07 06:30:28 +0200

    Refactor Controller

--- GameMaster/Controller.cs
+++ GameMaster/Controller.cs
@@ -1,5 +1,4 @@
 ﻿using GameMaster.Models;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -18,46 +17,27 @@ namespace GameMaster
 
         public List<Role> GetRoles()
         {
-            string query = "SELECT * FROM [@tableName] WHERE IsActive = 1;";
-            return _dbContext.Roles.FromSqlRaw(query, new SqlParameter("tableName", Constants.Tables.Role)).ToList();
+            return _dbContext.Roles.FromSqlInterpolated($"SELECT * FROM [Role] WHERE IsActive = 1;").ToList();
         }
 
         public int AddRole(string roleName)
         {
-            string query = "INSERT INTO [@tableName] (Name) VALUES(@rolename);";
-            return _dbContext.Database.ExecuteSqlRaw(query, new SqlParameter("tableName", Constants.Tables.Role),
-                new SqlParameter("rolename", roleName));
-        }
-
-        public User? GetUserByEmail(string email)
-        {
-            string query = "SELECT * FROM [@tableName] where Email = @email and IsActive = 1;";
-            return _dbContext.Users.FromSqlRaw(query, new SqlParameter("tableName", Constants.Tables.User),
-                new SqlParameter("email", email)).FirstOrDefault();
+            return _dbContext.Database.ExecuteSqlInterpolated($"INSERT INTO [Role] (Name) VALUES({roleName});");
         }
 
         public Person AddPerson(string firstName, string lastName, DateTime birthday)
         {
-            string query = "Exec NewPerson @firstName, @lastName, @birthday, @dateCreated);";
-            return _dbContext.People.FromSqlRaw(query, new SqlParameter("tableName", Constants.Tables.Person),
-                new SqlParameter("birthday", birthday), new SqlParameter("dateCreated", DateTime.UtcNow), 
-                new SqlParameter("firstName", firstName), new SqlParameter("lastName", lastName)).First();
+            return _dbContext.People.FromSqlInterpolated($"Exec NewPerson {firstName}, {lastName}, {birthday}, {DateTime.UtcNow};").First();
         }
 
-        public List<Person> GetPersonByName(string firstName, string lastName)
+        public User? GetUserByEmail(string email)
         {
-            string query = "SELECT * FROM [@tableName] WHERE FirstName = @firstName and LastName = @lastName;";
-            return _dbContext.People.FromSqlRaw(query, new SqlParameter("tableName", Constants.Tables.Person),
-                new SqlParameter("tableName", Constants.Tables.Person),
-                new SqlParameter("firstName", firstName), new SqlParameter("lastName", lastName)).ToList();
+            return _dbContext.Users.FromSqlInterpolated($"SELECT * FROM [User] where Email = {email} and IsActive = 1;").FirstOrDefault();
         }
 
         public int AddUser(string email, string password, int personId, int roleId)
         {
-            string query = "INSERT INTO [@tableName] (Email, Password, personId, RoleId) VALUES(@email, @password, @personId, @roleId)";
-            return _dbContext.Database.ExecuteSqlRaw(query, new SqlParameter("tableName", Constants.Tables.User),
-                new SqlParameter("email", email), new SqlParameter("password", password),
-                new SqlParameter("personId", personId), new SqlParameter("roleId", roleId));
+            return _dbContext.Database.ExecuteSqlInterpolated($"INSERT INTO [User] (Email, Password, personId, RoleId) VALUES({email}, {password}, {personId}, {roleId})");
         }
 
         public int NewUser(string firstName, string lastName, DateTime birthday, string email, string password, int roleId)
