cacebecc2a0f0d069b3c676a87a3508b63e09381
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-09 01:53:22 +0200

    Add Region Seeding

---
+++ GameMaster.Migration/Seeding/_102_SeedRegionTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace GameMaster.Migration.Seeding
+{
+    [Migration(102)]
+    public class _102_SeedRegionTable : FluentMigrator.Migration
+    {
+        public override void Down()
+        {
+        }
+
+        public override void Up()
+        {
+            Constants.Region.Regions.ForEach(region =>
+            {
+                Insert.IntoTable(Constants.Tables.Region).Row(new
+                {
+                    region.Id,
+                    region.Name
+                });
+            });
+        }
+    }
+}
