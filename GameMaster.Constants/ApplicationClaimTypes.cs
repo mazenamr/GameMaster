307c7aaf60ce0abb8b0e6388964518949f758a07
Muhab <icecream10800@gmail.com>
2021-01-08 22:26:52 +0200

    Added ApplicationClaimTypes

---
+++ GameMaster.Constants/ApplicationClaimTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace GameMaster.Constants
+{
+    public static class ApplicationClaimTypes
+    {
+        private const string Root = "./schemas/claims/";
+
+        public const string FirstName = Root + "FirstName";
+        public const string LastName = Root + "LastName";
+        public const string UserId = Root + "UserId";
+        public const string RoleId = Root + "RoleId";
+        public static string UserRoleName = "UserRoleName";
+    }
+}
