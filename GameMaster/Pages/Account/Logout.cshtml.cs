ca1b7478a3e83ec9e56d810eb036a516af607d5c
Muhab <icecream10800@gmail.com>
2021-01-08 04:22:17 +0200

    Logout

--- GameMaster/Pages/Account/Logout.cshtml.cs
+++ GameMaster/Pages/Account/Logout.cshtml.cs
@@ -2,15 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace GameMaster.Pages.Account
 {
     public class LogoutModel : PageModel
     {
+        private readonly ILogger<LogoutModel> _logger;
+
+        private readonly IHttpContextAccessor _httpContext;
+
+        public LogoutModel(ILogger<LogoutModel> logger, IHttpContextAccessor httpContext)
+        {
+            _logger = logger;
+            _httpContext = httpContext;
+        }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Index");
         }
     }
 }
