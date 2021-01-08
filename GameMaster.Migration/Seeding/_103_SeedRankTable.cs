569354fe9a25fdac2fbebefc0cf8e965a3b1d564
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-09 01:53:30 +0200

    Add Rank Seeding

---
+++ GameMaster.Migration/Seeding/_103_SeedRankTable.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace GameMaster.Migration.Seeding
+{
+    [Migration(103)]
+    public class _103_SeedRankTable : FluentMigrator.Migration
+    {
+        public override void Down()
+        {
+        }
+
+        public override void Up()
+        {
+            Constants.Rank.Ranks.ForEach(rank =>
+            {
+                Insert.IntoTable(Constants.Tables.Rank).Row(new
+                {
+                    rank.Id,
+                    rank.Name,
+                    rank.Score
+                });
+            });
+        }
+    }
+}
