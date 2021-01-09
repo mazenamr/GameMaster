77f8dd8a2315ed017be16a7675088efa6e64d96c
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-09 02:25:59 +0200

    Update Player Schema

--- GameMaster.Migration/Tables/_006_PlayerTable.cs
+++ GameMaster.Migration/Tables/_006_PlayerTable.cs
@@ -9,11 +9,12 @@ namespace GameMaster.Migration.Tables
         {
             Create.Table(Constants.Tables.Player)
                 .AutoId()
-                .WithColumn("Score").AsInt32().Nullable()
+                .WithColumn("Activity").AsInt32().NotNullable()
                 .WithColumn("Skill").AsInt32().NotNullable()
                 .WithColumn("Temper").AsInt32().NotNullable()
-                .IntForeignKeyIndexed("RegionId", Constants.Tables.Region, isNullable: false, isPK: false)
+                .WithColumn("Score").AsInt32().Nullable()
                 .IntForeignKeyIndexed("RankId", Constants.Tables.Rank, isNullable: false, isPK: false)
+                .IntForeignKeyIndexed("RegionId", Constants.Tables.Region, isNullable: false, isPK: false)
                 .IntForeignKeyIndexed("PersonId", Constants.Tables.Person, isNullable: false, isPK: false)
                 .WithColumn("DateCreated").AsDate().NotNullable()
                 .IsActive();
