6de91385fc9eada4aa1cf1c1d517fc875070be13
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-09 02:26:12 +0200

    Add NewPlayer Stored Procedure

--- GameMaster.Migration/StoredProcedures.sql
+++ GameMaster.Migration/StoredProcedures.sql
@@ -1,14 +1,14 @@
 CREATE PROCEDURE NewPerson @FirstName nvarchar(50), @LastName nvarchar(50), @Birthday date, @DateCreated datetime
 AS
 DECLARE @PersonOutput table(Id int,
-						   FirstName nvarchar(50),
-						   LastName nvarchar(50),
-						   Birthday date,
-						   DateCreated datetime,
-						   IsActive bit);
+                           FirstName nvarchar(50),
+                           LastName nvarchar(50),
+                           Birthday date,
+                           DateCreated datetime,
+                           IsActive bit);
 INSERT [Person] (FirstName, LastName, Birthday, DateCreated, IsActive)
-	OUTPUT INSERTED.Id, INSERTED.FirstName, INSERTED.LastName, INSERTED.Birthday, INSERTED.DateCreated, INSERTED.IsActive
-		INTO @PersonOutput
+    OUTPUT INSERTED.Id, INSERTED.FirstName, INSERTED.LastName, INSERTED.Birthday, INSERTED.DateCreated, INSERTED.IsActive
+        INTO @PersonOutput
 VALUES (@FirstName, @LastName, @Birthday, @DateCreated, 1);
 SELECT * FROM @PersonOutput;
 GO
@@ -16,40 +16,68 @@ GO
 CREATE PROCEDURE NewUser @FirstName nvarchar(50), @LastName nvarchar(50), @Birthday date, @DateCreated datetime, @Email nvarchar(100), @Username nvarchar(50), @Password nvarchar(100), @RoleId int
 AS
 DECLARE @PersonOutput table(Id int,
-						   FirstName nvarchar(50),
-						   LastName nvarchar(50),
-						   Birthday date,
-						   DateCreated datetime,
-						   IsActive bit);
+                           FirstName nvarchar(50),
+                           LastName nvarchar(50),
+                           Birthday date,
+                           DateCreated datetime,
+                           IsActive bit);
 INSERT [Person] (FirstName, LastName, Birthday, DateCreated, IsActive)
-	OUTPUT INSERTED.Id, INSERTED.FirstName, INSERTED.LastName, INSERTED.Birthday, INSERTED.DateCreated, INSERTED.IsActive
-		INTO @PersonOutput
+    OUTPUT INSERTED.Id, INSERTED.FirstName, INSERTED.LastName, INSERTED.Birthday, INSERTED.DateCreated, INSERTED.IsActive
+        INTO @PersonOutput
 VALUES (@FirstName, @LastName, @Birthday, @DateCreated, 1);
 DECLARE @UserOutput table(Id int,
-						  Email nvarchar(100),
-						  Username nvarchar(50),
-						  Password nvarchar(100),
-						  RoleId int,
-						  PersonId int,
-						  IsActive bit);
+                          Email nvarchar(100),
+                          Username nvarchar(50),
+                          Password nvarchar(100),
+                          RoleId int,
+                          PersonId int,
+                          IsActive bit);
 INSERT [User] (Email, Username, Password, RoleId, PersonId, IsActive)
-	OUTPUT INSERTED.Id, INSERTED.Email, INSERTED.Username, INSERTED.Password, INSERTED.RoleId, INSERTED.PersonId, INSERTED.IsActive
-		INTO @UserOutput
+    OUTPUT INSERTED.Id, INSERTED.Email, INSERTED.Username, INSERTED.Password, INSERTED.RoleId, INSERTED.PersonId, INSERTED.IsActive
+        INTO @UserOutput
 VALUES (@Email, @Username, @Password, @RoleId, (SELECT Id FROM @PersonOutput), 1);
 SELECT * FROM @UserOutput;
+GO
+
+CREATE PROCEDURE NewPlayer @FirstName nvarchar(50), @LastName nvarchar(50), @Birthday date, @DateCreated datetime, @Activity int, @Skill int, @Temper int, @Score int, @RankId int, @RegionId int
+AS
+DECLARE @PersonOutput table(Id int,
+                           FirstName nvarchar(50),
+                           LastName nvarchar(50),
+                           Birthday date,
+                           DateCreated datetime,
+                           IsActive bit);
+INSERT [Person] (FirstName, LastName, Birthday, DateCreated, IsActive)
+    OUTPUT INSERTED.Id, INSERTED.FirstName, INSERTED.LastName, INSERTED.Birthday, INSERTED.DateCreated, INSERTED.IsActive
+        INTO @PersonOutput
+VALUES (@FirstName, @LastName, @Birthday, @DateCreated, 1);
+DECLARE @PlayerOutput table(Id int,
+                           Activity int,
+                           Skill int,
+                           Temper int,
+                           Score int,
+                           RankId int,
+                           RegionId int,
+                           PersonId int,
+                           IsActive bit);
+INSERT [Player] (Activity, Skill, Temper, Score, RankId, RegionId, PersonId, IsActive)
+    OUTPUT INSERTED.Id, INSERTED.Activity, INSERTED.Skill, INSERTED.Temper, INSERTED.Score, INSERTED.RankId, INSERTED.RegionId, INSERTED.PersonId, INSERTED.IsActive
+        INTO @PlayerOutput
+VALUES (@Activity, @Skill, @Temper, @Score, @RankId, @RegionId, (SELECT Id FROM @PersonOutput), 1);
+SELECT * FROM @PlayerOutput;
 GO
 
 CREATE PROCEDURE NewWeapon @Name nvarchar(50), @Power int, @Speed int, @Block int
 AS
 DECLARE @WeaponOutput table(Id int,
-						   Name nvarchar(50),
-						   Power int,
-						   Speed int,
-						   Block int,
-						   IsActive bit);
+                           Name nvarchar(50),
+                           Power int,
+                           Speed int,
+                           Block int,
+                           IsActive bit);
 INSERT [Weapon] (Name, Power, Speed, Block, IsActive)
-	OUTPUT INSERTED.Id, INSERTED.Name, INSERTED.Power, INSERTED.Speed, INSERTED.Block, INSERTED.IsActive
-		INTO @WeaponOutput
+    OUTPUT INSERTED.Id, INSERTED.Name, INSERTED.Power, INSERTED.Speed, INSERTED.Block, INSERTED.IsActive
+        INTO @WeaponOutput
 VALUES (@Name, @Power, @Speed, @Block, 1);
 SELECT * FROM @WeaponOutput;
 GO
@@ -57,14 +85,14 @@ GO
 CREATE PROCEDURE NewCharacter @Name nvarchar(50), @Strength int, @Mobility int, @Health int
 AS
 DECLARE @CharacterOutput table(Id int,
-							   Name nvarchar(50),
-							   Strength int,
-							   Mobility int,
-							   Health int,
-							   IsActive bit);
+                               Name nvarchar(50),
+                               Strength int,
+                               Mobility int,
+                               Health int,
+                               IsActive bit);
 INSERT [Character] (Name, Strength, Mobility, Health, IsActive)
-	OUTPUT INSERTED.Id, INSERTED.Name, INSERTED.Strength, INSERTED.Mobility, INSERTED.Health, INSERTED.IsActive
-		INTO @CharacterOutput
+    OUTPUT INSERTED.Id, INSERTED.Name, INSERTED.Strength, INSERTED.Mobility, INSERTED.Health, INSERTED.IsActive
+        INTO @CharacterOutput
 VALUES (@Name, @Strength, @Mobility, @Health, 1);
 SELECT * FROM @CharacterOutput;
 GO
