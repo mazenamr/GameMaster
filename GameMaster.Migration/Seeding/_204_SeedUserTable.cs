0dba4f710074a9fa327953780a387ecead3d1e4a
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-09 02:40:25 +0200

    Add User Seeding

---
+++ GameMaster.Migration/Seeding/_204_SeedUserTable.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace GameMaster.Migration.Seeding
+{
+    [Migration(204)]
+    public class _204_SeedUserTable : FluentMigrator.Migration
+    {
+        internal class UserPassword
+        {
+            public static string HashPassword(string password)
+            {
+                return new PasswordHasher<UserPassword>().HashPassword(new(), password);
+            }
+        }
+
+        public override void Down()
+        {
+        }
+
+        public override void Up()
+        {
+            string password = UserPassword.HashPassword("1234567890");
+            Constants.Role.Roles.ForEach(role =>
+            {
+                string name = role.Name.ToLower();
+                Execute.Sql($"EXEC NewUser '{role.Name}', '{role.Name}', '2000-01-01', '{DateTime.UtcNow}', '{name}@gamemaster.xyz', '{name}', '{password}', {role.Id}");
+            });
+        }
+    }
+}
