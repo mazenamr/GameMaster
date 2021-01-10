1004738984ca6fb3e58f001e16223ec08addf763
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-10 06:58:18 +0200

    Add Weapon Seeding

---
+++ GameMaster.Migration/Seeding/_207_SeedWeaponTable.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace GameMaster.Migration.Seeding
+{
+    [Migration(207)]
+    public class _207_SeedWeaponTable : FluentMigrator.Migration
+    {
+        public override void Down()
+        {
+        }
+
+        public override void Up()
+        {
+            Constants.Weapon.Weapons.ForEach(weapon =>
+            {
+                Insert.IntoTable(Constants.Tables.Weapon).Row(new
+                {
+                    weapon.Name,
+                    weapon.Block,
+                    weapon.Power,
+                    weapon.Speed
+                });
+            });
+        }
+    }
+}
