6f84abf10f0512f5cdfd2fa07fc218f63c6c8433
Mazen Amr <14795777+mazenamr@users.noreply.github.com>
2021-01-10 07:11:24 +0200

    Fix Character Seeding

--- GameMaster.Constants/Character.cs
+++ GameMaster.Constants/Character.cs
@@ -9,7 +9,7 @@ namespace GameMaster.Constants
         public static CharacterInfo Archer = new("Archer", 200, 8, 8);
         public static CharacterInfo Assassin = new("Assassin", 500, 9, 5);
         public static CharacterInfo Knight = new("Knight", 1000, 5, 8);
-        public static CharacterInfo Wizard = new("Wizard", 500, 9, 5);
+        public static CharacterInfo Wizard = new("Wizard", 400, 3, 10);
 
         public static List<CharacterInfo> Characters = new()
         { Archer, Assassin, Knight, Wizard };
